44b5ba62847d52a9bbd378aea5c6d8b51635fa0b
itshizhan <itshizhan@163.com>
2018-10-31 22:29:00 +0800

    异常处理篇

---
+++ springboot/codes/exception-demo/src/main/java/com/itshizhan/ExceptionDemoApplication.java
@@ -0,0 +1,12 @@
+package com.itshizhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionDemoApplication.class, args);
+	}
+}
