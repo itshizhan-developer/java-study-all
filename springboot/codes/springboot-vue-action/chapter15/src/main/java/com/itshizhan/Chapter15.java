74503cc6305825d7e95a80508bfa94baf2bd87de
itshizhan <itshizhan@163.com>
2019-03-21 15:33:12 +0800

    springboot-vue-action

---
+++ springboot/codes/springboot-vue-action/chapter15/src/main/java/com/itshizhan/Chapter15.java
@@ -0,0 +1,12 @@
+package com.itshizhan;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter15 {
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter15.class,args);
+	}
+}
