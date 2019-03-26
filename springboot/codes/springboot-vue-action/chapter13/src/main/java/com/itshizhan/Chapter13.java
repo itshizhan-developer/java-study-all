b3b26ace9d25c3cc32be21dcb5cb3668307bd68b
itshizhan <itshizhan@163.com>
2019-03-26 17:03:30 +0800

    init chapter13 codes

---
+++ springboot/codes/springboot-vue-action/chapter13/src/main/java/com/itshizhan/Chapter13.java
@@ -0,0 +1,12 @@
+package com.itshizhan;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter13 {
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter13.class,args);
+	}
+}
