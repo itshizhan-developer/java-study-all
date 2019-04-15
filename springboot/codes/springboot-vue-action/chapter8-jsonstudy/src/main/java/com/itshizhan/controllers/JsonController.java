0d4a099ea0a572216ce2585298196a59453dd5e9
itshizhan <itshizhan@163.com>
2019-04-15 10:55:02 +0800

    pls ignore

--- springboot/codes/springboot-vue-action/chapter8-jsonstudy/src/main/java/com/itshizhan/controllers/JsonController.java
+++ springboot/codes/springboot-vue-action/chapter8-jsonstudy/src/main/java/com/itshizhan/controllers/JsonController.java
@@ -6,6 +6,9 @@ import com.itshizhan.beans.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -31,5 +34,40 @@ public class JsonController {
 
 		return new Student();
 
+	}
+
+	@GetMapping("/simpleJson")
+	public String simpleJson(){
+		ObjectMapper mapper = new ObjectMapper();
+
+		Map<String,Object> studentMap = new HashMap<>();
+		int[] marks = {1,2,3};
+		Student student = new Student();
+		student.setAge(18);
+		student.setName("Jack");
+		// JAVA Object
+		studentMap.put("student", student);
+		// JAVA String
+		studentMap.put("name", "Mahesh Kumar");
+		// JAVA Boolean
+		studentMap.put("verified", Boolean.FALSE);
+		// Array
+		studentMap.put("marks", marks);
+
+		String studentJson;
+
+
+		try {
+			studentJson = mapper.writeValueAsString(studentMap);
+			System.out.println(studentJson);
+			mapper.writeValue(new File("/Users/leeson8888/student/student.json"), studentMap);
+
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "simpleJson";
 	}
 }
