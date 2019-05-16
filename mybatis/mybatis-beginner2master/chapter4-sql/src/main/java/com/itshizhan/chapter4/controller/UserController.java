d157e21e1dc070c7f8d5a419f1f4481915b2ff1e
itshizhan <itshizhan@163.com>
2019-05-16 17:22:40 +0800

    mybatis-sql

--- mybatis/mybatis-beginner2master/chapter4-sql/src/main/java/com/itshizhan/chapter4/controller/UserController.java
+++ mybatis/mybatis-beginner2master/chapter4-sql/src/main/java/com/itshizhan/chapter4/controller/UserController.java
@@ -4,10 +4,9 @@ package com.itshizhan.chapter4.controller;
 import com.itshizhan.chapter4.bean.SysUser;
 import com.itshizhan.chapter4.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -40,5 +39,50 @@ public class UserController {
 	public SysUser selectByIdOrUserName(SysUser user){
 		return userMapper.selectByIdOrUserName(user);
 	}
+
+	@PostMapping("/selectByIdList1")
+	public List<SysUser> selectByIdList1(@RequestParam(value = "idList",required = false) List<Long> idList){
+//		List<Long> idList = new ArrayList();
+//		idList.add(1L);
+//		idList.add(2L);
+//		idList.add(1040L);
+		ArrayList<Long> arrayList = new ArrayList<>();
+		arrayList.add(1038L);
+		arrayList.add(1040L);
+		return userMapper.selectByIdList1(arrayList);
+	}
+
+	@PostMapping("/selectByIdList2")
+	public List<SysUser> selectByIdList2(@RequestParam(value = "idList",required = false) Long [] idList){
+		return userMapper.selectByIdList2(idList);
+	}
+
+	@PostMapping("/selectByIdList3")
+	public List<SysUser> selectByIdList3(@RequestParam() List<Long> idList1,
+																			 @RequestParam() List<Long> idList2){
+		return userMapper.selectByIdList3(idList1,idList2);
+	}
+
+	// 批量插入用户
+	@GetMapping("/insertUserByList")
+	public int insertUserByList(){
+		List<SysUser> userList = new ArrayList<>();
+		SysUser user1 = new SysUser();
+		user1.setUserName("test1_insert");
+		user1.setUserEmail("test1@163.com");
+		user1.setUserPassword("123456");
+		user1.setUserInfo("test1_insert_userInfo");
+
+		SysUser user2 = new SysUser();
+		user2.setUserName("test2_insert");
+		user2.setUserEmail("test2@163.com");
+		user2.setUserPassword("123456");
+		user2.setUserInfo("test2_insert_userInfo");
+
+		userList.add(user1);
+		userList.add(user2);
+		return userMapper.insertUserByList(userList);
+	}
+
 
 }
--- mybatis/mybatis-beginner2master/chapter4-sql/src/main/java/com/itshizhan/chapter4/mapper/UserMapper.java
+++ mybatis/mybatis-beginner2master/chapter4-sql/src/main/java/com/itshizhan/chapter4/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package com.itshizhan.chapter4.mapper;
 
 import com.itshizhan.chapter4.bean.SysUser;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,5 +17,21 @@ public interface UserMapper {
 	int insertUserSelective(SysUser sysUser) ;
 
 	SysUser selectByIdOrUserName(SysUser sysUser) ;
+
+	// 根据用户ids 集合批量查询用户
+	List<SysUser> selectByIdList(List<Long> idList);
+
+	// 根据用户ids 集合批量查询用户
+	List<SysUser> selectByIdList1(ArrayList<Long> idList);
+
+	// 根据用户ids 集合或数字批量查询用户
+	List<SysUser> selectByIdList2(Long [] idArray);
+
+	// 根据用户ids 集合或数字批量查询用户
+	List<SysUser> selectByIdList3(@Param("list1") List<Long> list1,
+																@Param("list2") List<Long> list2);
+
+	//批量插入用户
+	int insertUserByList(@Param("userList") List<SysUser> userList);
 
 }
