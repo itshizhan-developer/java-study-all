5db827bb9ce857feea6b91e2399fbc9f73a1189d
itshizhan <itshizhan@163.com>
2019-06-10 23:21:28 +0800

    design-pattern

--- design-pattern/graphic-design-pattern/3-template_method/src/main/java/com/itshizhan/templateMethod/AbstractDisplay.java
+++ design-pattern/graphic-design-pattern/3-template_method/src/main/java/com/itshizhan/templateMethod/AbstractDisplay.java
@@ -4,7 +4,7 @@ public abstract class AbstractDisplay {
 	abstract  void open();
 	abstract void print();
 	abstract void  close();
-	public final void  display(){
+	final void  display(){
 		open();
 		for (int i=0;i<5;i++){
 			print();
